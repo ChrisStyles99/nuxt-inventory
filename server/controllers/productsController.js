2a2a38c5f9f6757cf7a3d8fc58d85d20ce1278d0
ChrisStyles99 <chrisrdz99@hotmail.com>
2021-01-20 19:56:54 -0600

    Added add product and edit product routes

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -14,7 +14,28 @@ productsController.getSingleProduct = (req, res) => {
   connection.execute('SELECT * FROM products WHERE id = ?', [id], (err, results) => {
     if(err) return res.json({error: true, msg: err});
 
+    if(results.length < 1) return res.json({error: true, msg: "Couldn't find a product with that id"});
+
     res.json({error: false, product: results});
+  });
+}
+
+productsController.addProduct = (req, res) => {
+  const {name, description, quantity, url} = req.body;
+  connection.execute('INSERT INTO products (name, description, quantity, image_url) VALUES (?, ?, ?, ?)', [name, description, quantity, url], (err, result) => {
+    if(err) return res.json({error: true, msg: err});
+
+    res.json({error: false, msg: 'Added new product'});
+  });
+}
+
+productsController.editProduct = (req, res) => {
+  const {id} = req.params;
+  const {name, description, quantity, url} = req.body;
+  connection.execute('UPDATE products SET name = ?, description = ?, quantity = ?, image_url = ? WHERE id = ?', [name, description, quantity, url, id], (err, result) => {
+    if(err) return res.json({error: true, msg: err});
+
+    res.json({error: false, msg: 'Edited product successfully'});
   });
 }
 
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,11 +1,11 @@
 const {Router} = require('express');
-const { getAllProducts } = require('../controllers/productsController');
+const { getAllProducts, getSingleProduct, addProduct, editProduct } = require('../controllers/productsController');
 const router = Router();
 
 router.get('/', getAllProducts);
-router.get('/:id');
-router.post('/');
-router.put('/edit/:id');
+router.get('/:id', getSingleProduct);
+router.post('/', addProduct);
+router.put('/edit/:id', editProduct);
 router.delete('/delete/:id');
 
 module.exports = router;
