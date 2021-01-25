501d9f21c48cf0ed4fcb4393a51b314a706de253
ChrisStyles99 <chrisrdz99@hotmail.com>
2021-01-24 21:49:56 -0600

    Added cookie parser and cors packages

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const app = express();
 if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-// Db connection
-const connection = require('./database/db');
 
 // Port
 const PORT = process.env.PORT || 3001;
 
 // Middlewares
+app.use(cors({
+  origin: 'http://localhost:3000/',
+  credentials: true
+}));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use('/api/users', require('./routes/users'));
